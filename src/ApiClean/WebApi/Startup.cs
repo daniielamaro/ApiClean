af5d4d4cb674d09d7df5ed0a7fff7b5547a9e840
John-91 <jonvaladao91@gmail.com>
2019-11-08 14:10:47 -0300

    configuraçoes do Swagger no Startup

--- src/ApiClean/WebApi/Startup.cs
+++ src/ApiClean/WebApi/Startup.cs
@@ -2,13 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Rewrite;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NSwag;
 
 namespace WebApi
 {
@@ -22,10 +26,43 @@ namespace WebApi
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllers();
+
+            services.AddSwaggerDocument(document =>
+            {
+                document.Title = "DemoCleanArchitecture";
+                document.Version = "v1";
+                document.PostProcess = s =>
+                {
+                    s.Paths.ToList().ForEach(p =>
+                    {
+                        p.Value.Parameters.Add(
+                        new OpenApiParameter()
+                        {
+                            Kind = OpenApiParameterKind.Header,
+                            Type = NJsonSchema.JsonObjectType.String,
+                            IsRequired = false,
+                            Name = "Accept-Language",
+                            Description = "pt-BR or en-US",
+                            Default = "pt-BR"
+                        });
+                    });
+                };
+            });
+
+            var builder = new ContainerBuilder();
+           /* builder.RegisterModule<ApplicationModule>();
+            builder.RegisterModule<Infrastructure.PostgresDataAccess.Module>();
+            builder.RegisterModule<InfrastructureDefaultModule>();
+            builder.RegisterModule<WebApiModule>();
+            builder.Populate(services);*/
+
+            var container = builder.Build();
+            return new AutofacServiceProvider(container);
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -34,6 +71,11 @@ namespace WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwaggerUi3(config => config.TransformToExternalPath = (route, request) => ExtractPath(request) + route);
+            //Redireciona swagger como pagina inicial
+            var option = new RewriteOptions();
+            option.AddRedirect("^$", "swagger");
 
             app.UseMvc();
         }
