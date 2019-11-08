de7d8f46cd014a840cc79333ef4612405989764e
John-91 <jonvaladao91@gmail.com>
2019-11-08 17:21:35 -0300

    Criação da Pasta UseCases com a subpasta User e as sbpastas Add, Delete, Get,GetAll e Update e criaçao da classe UserResponses dentro de UseCases

---
+++ src/ApiClean/WebApi/UseCases/User/UserResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebApi.UseCases.User
+{
+    public class UserResponse
+    {
+        public Guid Id { get; private set; }
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Email { get; private set; }
+
+        public UserResponse(Guid id, string name, int age, string email)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+            Email = email;
+        }
+    }
+}
