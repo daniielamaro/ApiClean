e156d27368ee3705ec129c472c02497126af9ec5
daniielamaro <daniel.amaro.rj@gmail.com>
2019-11-08 11:39:01 -0300

    Domain sem o desenvolvimento das validações

---
+++ src/ApiClean/Domain/Entity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using FluentValidation.Results;
+using FluentValidation;
+
+namespace Domain
+{
+    public class Entity : IEntity
+    {
+        public Guid Id { get; set; }
+
+        [NotMapped]
+        public bool IsValid { get; private set; }
+
+        [NotMapped]
+        public ValidationResult ValidationResult { get; private set; }
+
+        public bool Validate<TEntity>(TEntity model, AbstractValidator<TEntity> validator)
+        {
+            ValidationResult = validator.Validate(model);
+            return IsValid = ValidationResult.IsValid;
+        }
+    }
+}
