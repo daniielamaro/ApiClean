4652ab5de21b6d52d32bbdfc87fc0997d9675d05
John-91 <jonvaladao91@gmail.com>
2019-11-08 15:49:51 -0300

    Criação da pasta Modules e classe WebApiModule

---
+++ src/ApiClean/WebApi/Modules/WebApiModule.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using DemoCleanArchitecture.WebApi.UseCases.Customer;
+
+namespace WebApi.Modules
+{
+    public class WebApiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterAssemblyTypes(typeof(Startup).Assembly)
+                .AsSelf().InstancePerLifetimeScope();
+
+            builder.RegisterType<CustomerPresenter>().As<Application.Boundaries.Customer.IOutPutPort>().AsSelf().InstancePerLifetimeScope();
+        }
+    }
+}
