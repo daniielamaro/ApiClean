391ed46e5b3e9e997e175af8c6d3ad12bcafc481
Thallitasp <thallitaspedreira@gmail.com>
2019-11-08 12:16:49 -0300

    Realocação das validações

---
+++ src/ApiClean/Domain/Validations/CommentExistValidator.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Validations
+{
+    class CommentExistValidator : AbstractValidator<Guid>
+    {
+        public CommentExistValidator()
+        {
+            commentRepository = new CommentRepository();
+
+            RuleFor(x => x)
+                .Cascade(CascadeMode.StopOnFirstFailure)
+                .NotNull()
+                .WithMessage("O id do comentário não pode ser nulo")
+                .NotEmpty()
+                .WithMessage("O id do comentário não pode ser vazio")
+                .Must(VerifyTopic)
+                .WithMessage("Este comentário não existe.");
+        }
+
+        private bool VerifyTopic(Guid id)
+        {
+            Comment comment = commentRepository.GetById(id);
+            return (comment != null);
+        }
+    }
+}
