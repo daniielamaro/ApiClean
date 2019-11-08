0afe5a4cdc44480af73987081b01467b1fd5cf9a
Mozart Maia <mozartthegame@gmail.com>
2019-11-08 15:31:49 -0300

    Application: Repositories e Boundaries

---
+++ src/ApiClean/Application/Boundaries/User/IOutputPort.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Application.Boundaries.User
+{
+    public interface IOutputPort
+    {
+        void Standard(Guid id);
+
+        void Standard(Domain.User.User user);
+
+        void Standard(IList<Domain.User.User> user);
+
+        void NotFound(string message);
+
+        void Error(string message);
+    }
+}
