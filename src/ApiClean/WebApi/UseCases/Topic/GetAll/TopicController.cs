825b97bd77f77d8895df232d8c13c8580abfcb6c
John-91 <jonvaladao91@gmail.com>
2019-11-11 17:04:49 -0300

    Criação das classes nas pastas Delete, Get, GetAll, Save no Topic

---
+++ src/ApiClean/WebApi/UseCases/Topic/GetAll/TopicController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Application.UseCases.Topic.GetAll;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.UseCases.Topic.GetAll
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TopicController : ControllerBase
+    {
+        private readonly TopicPresenter presenter;
+        private readonly ITopicGetAllUseCase topicGetAllUseCase;
+
+        public TopicController(TopicPresenter presenter, ITopicGetAllUseCase topicGetAllUseCase)
+        {
+            this.presenter = presenter;
+            this.topicGetAllUseCase = topicGetAllUseCase;
+        }
+
+        [HttpPost]
+        [Route("GetAllTopic")]
+        [ProducesResponseType(typeof(List<TopicResponse>), 200)]
+        [ProducesResponseType(typeof(ProblemDetails), 400)]
+        public IActionResult GetAllTopics()
+        {
+            topicGetAllUseCase.Execute();
+            return presenter.ViewModel;
+        }
+    }
+}
