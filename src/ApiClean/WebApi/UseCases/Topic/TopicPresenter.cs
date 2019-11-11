df8610242119c8f7780c3e6d8305281b8e026b87
John-91 <jonvaladao91@gmail.com>
2019-11-11 12:34:32 -0300

    Criaçao sub-pastas Topic

---
+++ src/ApiClean/WebApi/UseCases/Topic/TopicPresenter.cs
@@ -0,0 +1,41 @@
+﻿using ApiClean.Application.Boundaries.Customer;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace WebApi.UseCases.Topic
+{
+    public class UserPresenter : IOutputPort
+    {
+        public IActionResult ViewModel { get; private set; }
+
+        public void Error(string message)
+        {
+            var problemDetails = new ProblemDetails()
+            {
+                Title = "An error as ocurred",
+                Detail = message
+            };
+
+            ViewModel = new BadRequestObjectResult(problemDetails);
+        }
+
+        public void NotFound(string message)
+            => ViewModel = new NotFoundObjectResult(message);
+
+        public void Standard(Guid id)
+           => ViewModel = new OkObjectResult(id);
+
+        public void Standard(Domain.Customer.Customer customer)
+            => ViewModel = new OkObjectResult(new UserResponse(customer.Id, customer.Name, customer.Age, customer.Email));
+
+        public void Standard(IList<Domain.Customer.Customer> customer)
+        {
+            var customersResponse = new List<UserResponse>();
+            customer.ToList().ForEach(s => customersResponse.Add(new UserResponse(s.Id, s.Name, s.Age, s.Email)));
+            ViewModel = new OkObjectResult(customersResponse);
+        }
+    }
+}
