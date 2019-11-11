1e67607bd8c4caa2de0a5826efabf48218ff66b2
“raulsantiago” <raulsantiago@id.uff.br>
2019-11-11 09:36:37 -0300

    Inicio

---
+++ src/ApiClean/Application/UseCases/Topic/Save/Handler/ValidateHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.UseCases.Topic.Save.Handler
+{
+    class ValidateHandler : Handler<TopicSaveRequest>
+    {
+        public override void ProcessRequest(TopicSaveRequest request)
+        {
+            if (!request.Topic.IsValid)
+                throw new ArgumentException("Model invalid");
+
+            if (sucessor != null)
+                sucessor.ProcessRequest(request);
+        }
+    }
+}
---
+++ src/ApiClean/Domain/Validations/CommentValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Validations
+{
+    public class CommentValidator : AbstractValidator<Comment.Comment>
+    {
+    }
+}
