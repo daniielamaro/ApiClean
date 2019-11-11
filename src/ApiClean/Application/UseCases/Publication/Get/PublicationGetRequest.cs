453136beb65118f1c73269ece1a6db637dfe2627
Mozart Maia <mozartthegame@gmail.com>
2019-11-11 10:38:13 -0300

    Publication: UseCase

---
+++ src/ApiClean/Application/UseCases/Publication/Get/PublicationGetRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.UseCases.Publication.Get
+{
+    public class PublicationGetRequest
+    {
+        public Guid PubId { get; private set; }
+
+        public PublicationGetRequest(Guid pubId)
+        {
+            PubId = pubId;
+        }
+    }
+}
