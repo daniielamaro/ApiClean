34198d8e4b9a4cb520ba2e85dbb613d7ea67529c
Thallitasp <thallitaspedreira@gmail.com>
2019-11-11 14:15:32 -0300

    Domain - UseCase - User

---
+++ src/ApiClean/WebApi/UseCases/User/Add/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Application.UseCases.User.Save;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApi.UseCases.User.Add
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserPresenter presenter;
+        private readonly IUserSave userSaveUseCase;
+
+        public UserController(UserPresenter presenter, IUserSave userSave)
+        {
+            this.presenter = presenter;
+            this.userSaveUseCase = userSave;
+        }
+
+        [HttpPost]
+        [Route("CreateUser")]
+        [ProducesResponseType(typeof(Guid), 200)]
+        [ProducesResponseType(typeof(ProblemDetails), 400)]
+        public IActionResult CreateUser([FromBody] InputUser input)
+        {
+            var request = new UserSaveRequest(input.Name, input.Email, input.Password);
+            userSaveUseCase.Execute(request);
+            return presenter.ViewModel;
+        }
+    }
+}
