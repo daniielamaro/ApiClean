7b80afcbeab86056951669c37dff4df564cf3775
Mozart Maia <mozartthegame@gmail.com>
2019-11-11 14:49:49 -0300

    Teste: início dos testes

---
+++ src/ApiClean/XUnitTest/Cases/Application/User/Delete/UserDeleteUseCaseTests.cs
@@ -0,0 +1,55 @@
+﻿using Application.UseCases.User.Delete;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+using Xunit.Frameworks.Autofac;
+
+
+namespace XUnitTest.Cases.Application.User.Delete
+{
+    [UseAutofacTestFramework]
+    [TestCaseOrderer("DemoCleanArchitecture.Tests.TestCaseOrdering.PriorityOrderer", "DemoCleanArchitecture.Tests")]
+    class UserDeleteUseCaseTests
+    {
+        private readonly IUserDelete userDeleteUseCase;
+        private readonly CustomerPresenter presenter;
+        private readonly ICustomerWriteOnlyRepository customerWriteOnlyRepository;
+        private static Guid CustomerId;
+
+        public UserDeleteUseCaseTests(ICustomerDeleteUseCase customerDeleteUseCase, CustomerPresenter presenter, ICustomerWriteOnlyRepository customerWriteOnlyRepository)
+        {
+            this.customerDeleteUseCase = customerDeleteUseCase;
+            this.presenter = presenter;
+            this.customerWriteOnlyRepository = customerWriteOnlyRepository;
+        }
+
+        [Fact]
+        [TestPriority(1)]
+        public void ShouldAddSomeCustomer()
+        {
+            var model = CustomerBuilder.New().Build();
+            CustomerId = model.Id;
+            var ret = customerWriteOnlyRepository.Add(model);
+            ret.Should().Be(1);
+        }
+
+        [Fact]
+        [TestPriority(2)]
+        public void ShouldDeleteCustomer()
+        {
+            var request = new CustomerDeleteRequest(CustomerId);
+            customerDeleteUseCase.Execute(request);
+            presenter.ViewModel.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        [TestPriority(2)]
+        public void ShouldDeleteCustomerAndReturnError()
+        {
+            var request = new CustomerDeleteRequest(Guid.NewGuid());
+            customerDeleteUseCase.Execute(request);
+            presenter.ViewModel.Should().BeOfType<BadRequestObjectResult>();
+        }
+    }
+}
